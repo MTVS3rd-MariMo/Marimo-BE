e0e4529853a12093a5bf51b253e07b2b14a0a8b2
JungHyeonmin <2ee42a45@gmail.com>
2024-12-13 20:02:30 +0900

    Feat : conf파일 추가, 더이데이터 만복이로 변경

--- src/main/java/com/todock/marimo/domain/lesson/service/AvatarService.java
+++ src/main/java/com/todock/marimo/domain/lesson/service/AvatarService.java
@@ -113,8 +113,8 @@ public class AvatarService {
 
         AvatarResponseDto avatarResponseDto;
 
-        if (userId == 1L) { // 빨간모자
-            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(2L, userId)
+        if (userId == 1L) { // 만복이
+            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(3L, userId)
                     .orElseThrow(() -> new EntityNotFoundException("lessonId : 1, userId 1로 빨간모자 아바타를 찾을 수 없습니다."));
 
             // 기존 Animation을 복사하여 새로운 Animation 생성
@@ -145,11 +145,11 @@ public class AvatarService {
             lessonRepository.save(lesson);
 
             avatarResponseDto = new AvatarResponseDto(newAvatar.getUserId(), newAvatar.getAvatarImg(), newAvatar.getAnimations());
-            avatar.setLesson(lessonRepository.findById(2L).orElseThrow(() -> new EntityNotFoundException("lessonId 2로 변경 실패")));
+            avatar.setLesson(lessonRepository.findById(3L).orElseThrow(() -> new EntityNotFoundException("lessonId 2로 변경 실패")));
             return avatarResponseDto;
 
-        } else if (userId == 2L) { // 할머니
-            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(2L, userId)
+        } else if (userId == 2L) { // 만복이엄마
+            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(3L, userId)
                     .orElseThrow(() -> new EntityNotFoundException("lessonId : 1, userId 2로 할머니 아바타를 찾을 수 없습니다."));
 
             // 기존 Animation을 복사하여 새로운 Animation 생성
@@ -180,11 +180,11 @@ public class AvatarService {
             lessonRepository.save(lesson);
 
             avatarResponseDto = new AvatarResponseDto(newAvatar.getUserId(), newAvatar.getAvatarImg(), newAvatar.getAnimations());
-            avatar.setLesson(lessonRepository.findById(2L).orElseThrow(() -> new EntityNotFoundException("lessonId 1로 변경 실패")));
+            avatar.setLesson(lessonRepository.findById(3L).orElseThrow(() -> new EntityNotFoundException("lessonId 1로 변경 실패")));
             return avatarResponseDto;
 
-        } else if (userId == 3L) { // 늑대
-            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(2L, userId)
+        } else if (userId == 3L) { // 선생님
+            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(3L, userId)
                     .orElseThrow(() -> new EntityNotFoundException("lessonId : 1, userId 3로 늑대 아바타를 찾을 수 없습니다."));
 
             // 기존 Animation을 복사하여 새로운 Animation 생성
@@ -215,11 +215,11 @@ public class AvatarService {
             lessonRepository.save(lesson);
 
             avatarResponseDto = new AvatarResponseDto(newAvatar.getUserId(), newAvatar.getAvatarImg(), newAvatar.getAnimations());
-            avatar.setLesson(lessonRepository.findById(2L).orElseThrow(() -> new EntityNotFoundException("lessonId 1으로 변경 실패")));
+            avatar.setLesson(lessonRepository.findById(3L).orElseThrow(() -> new EntityNotFoundException("lessonId 1으로 변경 실패")));
             return avatarResponseDto;
 
-        } else if (userId == 4L) { // 사냥꾼
-            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(2L, userId)
+        } else if (userId == 4L) { // 은지
+            Avatar avatar = avatarRepository.findByLesson_LessonIdAndUserId(3L, userId)
                     .orElseThrow(() -> new EntityNotFoundException("lessonId : 1, userId 2로 사냥꾼 아바타를 찾을 수 없습니다."));
 
             // 기존 Animation을 복사하여 새로운 Animation 생성
@@ -250,7 +250,7 @@ public class AvatarService {
             lessonRepository.save(lesson);
 
             avatarResponseDto = new AvatarResponseDto(newAvatar.getUserId(), newAvatar.getAvatarImg(), newAvatar.getAnimations());
-            avatar.setLesson(lessonRepository.findById(2L).orElseThrow(() -> new EntityNotFoundException("lessonId 1로 변경 실패")));
+            avatar.setLesson(lessonRepository.findById(3L).orElseThrow(() -> new EntityNotFoundException("lessonId 1로 변경 실패")));
             return avatarResponseDto;
 
         }
