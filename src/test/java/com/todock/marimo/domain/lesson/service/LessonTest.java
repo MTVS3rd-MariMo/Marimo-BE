18e663ebc30cf678a31f6f6614ec2fdb9b6eff5f
JungHyeonmin <2ee42a45@gmail.com>
2025-01-19 22:31:46 +0900

    Feat: 테스트 코드 클래스 추가

---
+++ src/test/java/com/todock/marimo/domain/lesson/service/LessonTest.java
@@ -0,0 +1,7 @@
+package com.todock.marimo.domain.lesson.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LessonTest {
+}
---
+++ src/test/java/com/todock/marimo/domain/result/service/ResultServiceTest.java
@@ -0,0 +1,7 @@
+package com.todock.marimo.domain.result.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ResultServiceTest {
+}
